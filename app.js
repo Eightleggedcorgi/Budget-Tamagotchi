066e71150060c2da55b35478a2b2646ecc66e4e0
Daniel Fordham <dfordham46@gmail.com>
2024-04-24 19:45:27 -0400

    Aging and images added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //look back at oop in lessons or hw6 landscaper for tips
-const dedButton = document.querySelector(".button.ded");
-const fedButton = document.querySelector(".button.fed");
+//const dedButton = document.querySelector(".button.ded");
+//const fedButton = document.querySelector(".button.fed");
 
 document.querySelector(".js-fed-button").onclick = function () {
     alert("Yum yum");
@@ -11,7 +11,7 @@ document.querySelector(".js-rest-button").onclick = function () {
 }
 
 document.querySelector(".js-play-button").onclick = function () {
-    alert("Yippee!");
+    alert("Yippee!"); //should make play transform the image left to right
 }
 
 document.querySelector(".js-ded-button").onclick = function () {
@@ -22,7 +22,7 @@ document.querySelector(".js-ded-button").onclick = function () {
         updateDisplay()
     } */
 
-class Tama {
+class Game {
     constructor(name, birthday, color, eyecolor, attitude, favfood, altfavfood) {
     this.name = name //can be string or integer
     this.birthday = birthday //better be an integer
@@ -50,4 +50,45 @@ class Tama {
 
 };
 
-const bungo = new Tama('Bungo', 1977, 'Shrimp Colors', 'Shrimpier Colors', 'Inhuman', 'Human Hearts', 'Pig Snouts');
+let gatoHunger = 1;
+let gatoSleep = 1;
+let gatoBored = 1;
+let gatoAge = 1; //should be the inputted age, 1 is temporary
+let doomTimer = 0;
+
+/* for (i = 0; i < 15; i++) {
+    setTimeout (12000)
+    doomTimer += 1;
+    alert("You feel older")
+} fires all the messages at once */
+/* setTimeout(() => {
+    doomTimer += 1;
+    alert("You feel older")
+}, 8000) works but only fires once*/
+/* const timer = () => {
+    setTimeout(8000);
+    doomTimer += 1;
+    alert("Your pet is older")
+} */
+/* for (i = 0; i <15; i++) {
+ timer()
+} */
+
+let counted = 0;
+let target = 15;
+let i = setInterval(function () {
+    if (counted <= target) {
+        counted++;
+        doomTimer += 1;
+        alert("Your pet is older")
+    } else {
+        clearInterval(i);
+    }
+}, 5000); //IT WORKS HOORAAAAAAAAAAAAAAAAAAAA interval 5000 = 5 seconds
+
+if (doomTimer >= 15) {
+    alert("ANNIHILATION APPROACHES") //get this to work
+}
+
+const bungo = new Game('Bungo', 1977, 'Shrimp Colors', 'Shrimpier Colors', 'Inhuman', 'Human Hearts', 'Pig Snouts');
+//console.log(bungo)
