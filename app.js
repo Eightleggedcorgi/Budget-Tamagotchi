16edd6ae4fbf4bf377fb90361cc597876e040c63
Daniel Fordham <dfordham46@gmail.com>
2024-04-29 17:51:31 -0400

    death mechanics done, now just animations left

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ let gatoHunger = 0;
 let gatoSleep = 0;
 let gatoBored = 0;
 let gatoAge = 1; //should be the inputted age, 1 is temporary
+let deathActive = false;
 //let doomTimer = 0;
 
 //button functions
 document.querySelector(".js-fed-button").onclick = function () {
+    if (deathActive === true) {
+        gatoHunger += 0;
+    }
+    else {
     alert("Yum yum");
-    gatoHunger -= 2;
+    gatoHunger -= 2; }
     if (gatoHunger < 0) {
         gatoHunger = 0;
     }
@@ -16,8 +21,12 @@ document.querySelector(".js-fed-button").onclick = function () {
 }
 
 document.querySelector(".js-rest-button").onclick = function () {
+    if (deathActive === true) {
+        gatoHunger += 0;
+    }
+    else {
     alert("Zzzzzz");
-    gatoSleep -= 2;
+    gatoSleep -= 2; }
     if (gatoSleep < 0) {
         gatoSleep = 0;
     }
@@ -25,8 +34,12 @@ document.querySelector(".js-rest-button").onclick = function () {
 }
 
 document.querySelector(".js-play-button").onclick = function () {
+    if (deathActive === true) {
+        gatoHunger += 0;
+    }
+    else {
     alert("Yippee!"); //should make play transform the image left to right
-    gatoBored -= 1;
+    gatoBored -= 1; }
     if (gatoBored < 0) {
         gatoBored = 0;
     }
@@ -35,28 +48,42 @@ document.querySelector(".js-play-button").onclick = function () {
 
 document.querySelector(".js-ded-button").onclick = function () {
     alert("PERISH");
+    deathActive = true;
+    updateDeathDisplay();
 }
 
 //SUPER button functions
 document.querySelector(".js-superfed-button").onclick = function () {
+    if (deathActive === true) {
+        gatoHunger += 0;
+    }
+    else {
     alert("MMMMMM YYYYYYYYYYUM YUUUUUUUUMMMMMM");
-    gatoHunger -= 4;
+    gatoHunger -= 4; }
     if (gatoHunger < 0) {
         gatoHunger = 0;
     }
     updateHungDisplay();
 }
 document.querySelector(".js-superrest-button").onclick = function () {
-    alert("zzzzzZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
-    gatoSleep -= 3;
+    if (deathActive === true) {
+        gatoHunger += 0;
+    }
+    else {
+    alert("zzzzzZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
+    gatoSleep -= 3; }
     if (gatoSleep < 0) {
         gatoSleep = 0;
     }
     updateTiredDisplay();
 }
 document.querySelector(".js-superplay-button").onclick = function () {
+    if (deathActive === true) {
+        gatoHunger += 0;
+    }
+    else {
     alert("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"); //perfect length
-    gatoBored -= 3;
+    gatoBored -= 3; }
     if (gatoBored < 0) {
         gatoBored = 0;
     }
@@ -67,21 +94,49 @@ document.querySelector(".js-superplay-button").onclick = function () {
     let counterHungDisplay = document.querySelector('.output1')
     function updateHungDisplay(){
         counterHungDisplay.innerHTML = gatoHunger;
+        if (gatoHunger >= 10) {
+            alert("your pet has DIED OF HUNGER")
+            deathActive = true;
+            updateDeathDisplay();
+            gatoHunger -= 1;
+        }
     };
 
     let counterTiredDisplay = document.querySelector('.output2')
     function updateTiredDisplay(){
         counterTiredDisplay.innerHTML = gatoSleep;
+        if (gatoSleep >= 10) {
+            alert("your pet has DIED OF EXHAUSTION")
+            deathActive = true;
+            updateDeathDisplay();
+            gatoSleep -= 1;
+        }
     };
 
     let counterBoredDisplay = document.querySelector('.output3')
     function updateBoredDisplay(){
         counterBoredDisplay.innerHTML = gatoBored;
+        if (gatoBored >= 10) {
+            alert("your pet has DIED OF BOREDOM")
+            deathActive = true;
+            updateDeathDisplay();
+            gatoBored -= 1;
+        }
     };
 
     let counterAgeDisplay = document.querySelector('.output4')
     function updateAgeDisplay(){
         counterAgeDisplay.innerHTML = gatoAge;
+    };
+
+    let counterDeathDisplay = document.querySelector('.isdead')
+    function updateDeathDisplay(){
+        if (deathActive === true) {
+        counterDeathDisplay.innerHTML = "DEAD.";
+        }
+        else {
+            counterDeathDisplay.innerHTML = "Alive!";
+        }
     };
 
 //Evil insidious timers of entropy
@@ -90,10 +145,20 @@ let target = 15;
 let i = setInterval(function () {
     if (counted <= target) {
             counted++;
-            gatoAge += 1;
+            if (deathActive === true) {
+                gatoAge += 0;
+            }
+            else {
+                gatoAge += 1;
+            }
             updateAgeDisplay()
             alert("Habby Borfday! Your pet is older") //might make age meter take inputted name. depends on headache required.
-            if (gatoAge >= 15) {
+            if (gatoAge >= 14) {
+                alert("ANNNNNNNNNNIHILATION!!!!!")
+                deathActive = true;
+                updateDeathDisplay();
+            }
+            else if (gatoAge >= 10) {
                 alert("ANNIHILATION APPROACHES")
             }
         } else {
@@ -106,7 +171,12 @@ let target2 = 600;
 let i2 = setInterval(function () {
     if (counted2 <= target2) {
             counted2++;
-            gatoHunger += 1;
+            if (deathActive === true) {
+                gatoHunger += 0;
+            }
+            else {
+                gatoHunger += 1;
+            }
             updateHungDisplay()
         } else {
             clearInterval(i);
@@ -118,7 +188,12 @@ let target3 = 600;
 let i3 = setInterval(function () {
     if (counted3 <= target3) {
             counted3++;
-            gatoSleep += 1;
+            if (deathActive === true) {
+                gatoSleep += 0;
+            }
+            else {
+                gatoSleep += 1;
+            }
             updateTiredDisplay()
         } else {
             clearInterval(i);
@@ -130,7 +205,12 @@ let target4 = 600;
 let i4 = setInterval(function () {
     if (counted4 <= target4) {
             counted4++;
-            gatoBored += 1;
+            if (deathActive === true) {
+                gatoBored += 0;
+            }
+            else {
+                gatoBored += 1;
+            }
             updateBoredDisplay()
         } else {
             clearInterval(i);
@@ -167,23 +247,5 @@ class Game {
 
 };
 
-/* for (i = 0; i < 15; i++) {
-    setTimeout (12000)
-    doomTimer += 1;
-    alert("You feel older")
-} fires all the messages at once */
-/* setTimeout(() => {
-    doomTimer += 1;
-    alert("You feel older")
-}, 8000) works but only fires once*/
-/* const timer = () => {
-    setTimeout(8000);
-    doomTimer += 1;
-    alert("Your pet is older")
-} */
-/* for (i = 0; i <15; i++) {
- timer()
-} */
-
-const bungo = new Game('Bungo', 1977, 'Shrimp Colors', 'Shrimpier Colors', 'From Beyond Space & Time', 'Human Hearts', 'Pig Snouts');
+const bungo = new Game('Bungo', 1412, 'Shrimp Colors', 'Shrimpier Colors', 'From Beyond Space & Time', 'Human Hearts', 'Pig Snouts');
 //console.log(bungo)
